73d5570692406a472bc188d7407229375373ca3a
AzamatNaka <azamatn410@gmail.com>
2023-03-01 14:08:15 +0600

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+//return view with all posts
+    public function index(){
+        $allPosts = Post::all();
+        return view('posts.index', ['posts' => $allPosts]);
+    }
+
+    //return view with a form create
+    public function create(){
+        return view('posts.create');
+    }
+
+    //save a post with title and content to DATABASE
+    public function store(Request $req){
+//        return $req->content;
+        Post::create([
+            'title' => $req->title,
+            'content' => $req->content,
+        ]);
+        return redirect()->route('posts.index');
+    }
+
+    //return view with a form create
+    public function show(Post $post){
+//        $post = Post::find($id); == Post $post
+        return view('posts.show', ['post' => $post]);
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit', ['post' => $post]);
+    }
+
+
+    public function update(Request $request, Post $post)
+    {
+        $post->update([
+            'title' => $request->title,     //$request->title == $request->input('title')
+            'content' => $request->content, //$request->content == $request->input('content')
+        ]);
+
+        return redirect()->route('posts.index');
+    }
+
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('posts.index');
+    }
+}
